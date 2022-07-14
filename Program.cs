07c47d27b1ec4407e8dbf728e5062a99dbd58fcb
Алиса <petrunina.alisa@gmail.com>
2022-07-14 22:07:09 +0300

    49+51

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ int columns=Convert.ToInt32(Console.ReadLine());
 
 int[,] array = GetArray(rows, columns, 0, 7);
 PrintArray(array);
-*/
+
 
 //Задача 48
 
@@ -72,3 +72,96 @@ int columns=Convert.ToInt32(Console.ReadLine());
 
 int[,] array = GetArray(rows, columns);
 PrintArray(array);
+*/
+
+//49
+
+Console.Clear();
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=Convert.ToInt32(Console.ReadLine());
+int[,] array = GetArray(rows, columns, 2, 10);
+PrintArray(array);
+Console.WriteLine("----------------------- ");
+for (int i = 0; i < rows; i = i + 2)
+    {
+        for (int j = 0; j < columns; j = j + 2)
+{
+    array[i,j] = array[i,j] * array[i,j];
+}}
+    
+    
+PrintArray(array);
+/*
+//51
+int[,] GetArray(int m, int n, int minValue, int maxValue) // создает двумерный массив
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+    
+}
+int GetSum (int [,] array) 
+{   int sum=0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i==j) sum=sum+array[i,j];
+        }
+    }
+    return sum;
+} 
+void PrintArray(int[,] inArray) // вывводит двумерный массив
+{
+    for (int row = 0; row < inArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < inArray.GetLength(1); column++)
+        {
+            Console.Write($"{inArray[row,column]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+*/
