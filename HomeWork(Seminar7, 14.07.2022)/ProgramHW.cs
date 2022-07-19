91510eb90bf26d141ed128af96518269f2d6b3ab
Алиса <petrunina.alisa@gmail.com>
2022-07-19 21:13:50 +0300

    Задача 47

--- HomeWork(Seminar7, 14.07.2022)/ProgramHW.cs
+++ HomeWork(Seminar7, 14.07.2022)/ProgramHW.cs
@@ -1,6 +1,42 @@
 ﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-m = 3, n = 4.
-0,5  7    -2  -0,2
-1   -3,3  8   -9,9
-8    7,8  -7,1  9
+
+
+Console.Clear();
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble()*(maxValue - minValue)+minValue;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int row = 0; row < inArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < inArray.GetLength(1); column++)
+        {
+            Console.Write($"{inArray[row, column]:f3}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=Convert.ToInt32(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns, 1, 7);
+PrintArray(array);
 */
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
