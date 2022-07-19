d24d94163763739edfe45ddb0a47ddabef98ee08
Алиса <petrunina.alisa@gmail.com>
2022-07-19 23:46:57 +0300

    52

--- HomeWork(Seminar7, 14.07.2022)/ProgramHW.cs
+++ HomeWork(Seminar7, 14.07.2022)/ProgramHW.cs
@@ -89,7 +89,55 @@ if (a< rows && b< columns){
 else {
     Console.WriteLine("Такой элемент отсутствует");
 }
+
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Clear();
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int row = 0; row < inArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < inArray.GetLength(1); column++)
+        {
+            Console.Write($"{inArray[row,column]}     ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 7);
+PrintArray(array);
+
+for (int j = 0; j < array.GetLength(1); j++)
+    {double sum =0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {sum = sum + array[i,j];
+        }
+    Console.Write($"{(sum/rows):f2}  ");
+    }
 */
+
 
 
 
